10dc877436b21d219b44f255e3f935859be3ef34
Choi-JJunho <junho5336@gmail.com>
2023-05-27 17:39:17 +0900

    feat: DB 사용자 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,24 @@
-CREATE TABLE member
+CREATE TABLE IF NOT EXISTS member
 (
     id        BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email     VARCHAR(255) NOT NULL UNIQUE,
+    nickname  VARCHAR(255) NOT NULL,
     password  VARCHAR(255) NOT NULL,
     role      VARCHAR(255) NOT NULL,
     image_url TEXT         NOT NULL
 );
 
-CREATE TABLE notice
+CREATE TABLE IF NOT EXISTS notice
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title       VARCHAR(255) NOT NULL UNIQUE,
     description TEXT         NOT NULL,
-    created_at  DATETIME     NOT NULL,
+    created_at  TIMESTAMP    NOT NULL,
     writer_id   BIGINT       NOT NULL,
     FOREIGN KEY (writer_id) REFERENCES member (id)
 );
 
-CREATE TABLE image
+CREATE TABLE IF NOT EXISTS image
 (
     id        BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     notice_id BIGINT NOT NULL,
---
+++ src/test/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT IGNORE INTO member(email, nickname, password, role, image_url)
+VALUES ('aa@aa.com', '로피', '1234', 'USER',
+        'https://img.freepik.com/free-photo/adorable-kitty-looking-like-it-want-to-hunt_23-2149167099.jpg?w=2000');
+
+INSERT IGNORE INTO member(email, nickname, password, role, image_url)
+VALUES ('bb@bb.com', '링링', '1234', 'USER',
+        'https://newsimg.hankookilbo.com/2017/11/06/201711060912547465_1.jpg');
+
+INSERT IGNORE INTO member(email, nickname, password, role, image_url)
+VALUES ('cc@cc.com', '하티', '1234', 'USER',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMTAyMDRfNjIg/MDAxNjEyNDA4OTk5NDQ4.6UGs399-0EXjIUwwWsYg7o66lDb-MPOVQ-zNDy1Wnnkg.m-WZz0IKKnc5OO2mjY5dOD-0VsfpXg7WVGgds6fKwnIg.JPEG.sunny_side_up12/1612312679152%EF%BC%8D2.jpg?type=w800');
+
+INSERT IGNORE INTO member(email, nickname, password, role, image_url)
+VALUES ('dd@dd.com', '관리자', '1234', 'ADMIN',
+        'https://t1.daumcdn.net/news/202105/22/petzzi/20210522141352278yebt.jpg');

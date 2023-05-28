57ed1b758a65203d28de2bb2947f8cffc59adaa1
thdwoqor <celebarte01@gmail.com>
2023-05-28 20:54:45 +0900

    test: 테스트시 SlackService Mocking

--- src/test/kotlin/powerfive/integration/IntegrationTest.kt
+++ src/test/kotlin/powerfive/integration/IntegrationTest.kt
@@ -3,13 +3,18 @@ package powerfive.integration
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.annotation.DirtiesContext
+import powerfive.util.SlackService
 
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTest {
+
+    @MockBean
+    private lateinit var restTemplate: SlackService
 
     @LocalServerPort
     var port = 0

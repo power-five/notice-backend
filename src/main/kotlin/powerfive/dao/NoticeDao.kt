e6d5cbf94cd1a298e9ec049673799be384b2faf1
Choi-JJunho <junho5336@gmail.com>
2023-06-03 09:12:10 +0900

    refactor: 게시물 조회 시 최신순으로 정렬

--- src/main/kotlin/powerfive/dao/NoticeDao.kt
+++ src/main/kotlin/powerfive/dao/NoticeDao.kt
@@ -8,35 +8,35 @@ import powerfive.entity.NoticeEntity
 
 @Component
 class NoticeDao(
-        private val jdbcTemplate: JdbcTemplate,
+    private val jdbcTemplate: JdbcTemplate,
 ) {
     private val simpleJdbcInsert: SimpleJdbcInsert = SimpleJdbcInsert(jdbcTemplate)
-            .withTableName("notice")
-            .usingGeneratedKeyColumns("id")
+        .withTableName("notice")
+        .usingGeneratedKeyColumns("id")
 
     fun findById(id: Long): NoticeEntity {
-        val sql = "select * from notice where id = ?"
+        val sql = "SELECT * FROM notice WHERE id = ?"
         return jdbcTemplate.queryForObject(sql, mapper, id) ?: throw NoSuchElementException("게시물을 찾을 수 없습니다.")
     }
 
     fun findAll(): List<NoticeEntity> {
-        val sql = "select * from notice"
+        val sql = "SELECT * FROM notice ORDER BY created_at DESC"
         return jdbcTemplate.query(sql, mapper)
     }
 
     fun insert(noticeEntity: NoticeEntity): Long {
         val map: Map<String, Any> = mapOf(
-                "title" to noticeEntity.title,
-                "description" to noticeEntity.description,
-                "created_at" to noticeEntity.createAt,
-                "writer_id" to noticeEntity.writerId
+            "title" to noticeEntity.title,
+            "description" to noticeEntity.description,
+            "created_at" to noticeEntity.createAt,
+            "writer_id" to noticeEntity.writerId
         )
         return simpleJdbcInsert.executeAndReturnKey(map).toLong()
     }
 
     fun update(noticeEntity: NoticeEntity) {
-        val sql = "UPDATE notice SET title = ?, description = ? WHERE id = ?";
-        jdbcTemplate.update(sql, noticeEntity.title, noticeEntity.description, noticeEntity.id);
+        val sql = "UPDATE notice SET title = ?, description = ? WHERE id = ?"
+        jdbcTemplate.update(sql, noticeEntity.title, noticeEntity.description, noticeEntity.id)
     }
 
     fun deleteById(id: Long) {
@@ -47,11 +47,11 @@ class NoticeDao(
     companion object {
         val mapper = RowMapper<NoticeEntity> { resultSet, rowId ->
             NoticeEntity(
-                    resultSet.getLong("id"),
-                    resultSet.getString("title"),
-                    resultSet.getString("description"),
-                    resultSet.getTimestamp("created_at"),
-                    resultSet.getLong("writer_id")
+                resultSet.getLong("id"),
+                resultSet.getString("title"),
+                resultSet.getString("description"),
+                resultSet.getTimestamp("created_at"),
+                resultSet.getLong("writer_id")
             )
         }
     }
